25b885974c00676d94c70e7cfdd57e2c30797905
hz <hz@archlinux.local>
2024-11-17 18:19:47 +0100

    did stuff

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,36 @@ use uom::ConstantOp::Add;
 use uom::num::Num;
 
 mod calc;
+mod asynch;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native("Link Budget Calculator", native_options, Box::new(|cc| LinkBudgetApp::new(cc))).unwrap();
 }
 
-#[derive(Default)]
+#[derive(Default, PartialEq, Eq)]
+enum CalculationTarget {
+    Temperature,
+    Bandwidth,
+    #[default]
+    Snr,
+    NoiseFigure,
+    TxGain,
+    RxGain,
+    Distance,
+    AdditionalLosses,
+    TxPower,
+    RxPower,
+}
+
 struct LinkBudgetApp {
     temperature: f64, // Kelvin
     bandwidth: f64, // Hertz
     snr_min: f64, // dB
     noise_figure: f64, // dB
 
+    tx_power: f64, // dBm
+    rx_power: f64, // dBm
     tx_gain: f64, // dB
     rx_gain: f64, // dB
 
@@ -31,6 +48,30 @@ struct LinkBudgetApp {
     additional_losses: Vec<AdditionalLoss>,
     loss_name: String,
     loss_db: f64,
+
+    calculation_target: CalculationTarget,
+}
+
+impl Default for LinkBudgetApp {
+    fn default() -> Self {
+        Self {
+            temperature: 290.0,
+            bandwidth: 20e6,
+            snr_min: 10.0,
+            noise_figure: 10.0,
+            tx_gain: 5.0,
+            rx_gain: 5.0,
+            tx_power: 30.0,
+            rx_power: 1.0,
+            distance: 2000.0,
+            d_break: 500.0,
+            break_exponent: 4.3,
+            additional_losses: Vec::default(),
+            loss_name: String::default(),
+            loss_db: 10.0,
+            calculation_target: CalculationTarget::default(),
+        }
+    }
 }
 
 struct AdditionalLoss {
@@ -47,36 +88,52 @@ impl LinkBudgetApp {
 impl eframe::App for LinkBudgetApp {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Minimal transmission power");
+            ui.heading("Link Budget Calculator");
             frame_styled(ui).show(ui, |ui| {
                 ui.heading("Base Info");
                 egui::Grid::new("base_data").num_columns(2).show(ui, |ui| {
-                    ui.label("Temperature");
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Temperature, "Temperature");
                     ui.add(DragValue::new(&mut self.temperature).suffix(" K"));
                     ui.end_row();
 
-                    ui.label("Bandwidth");
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Bandwidth, "Bandwidth");
                     ui.add(prefix_drag_value(&mut self.bandwidth).suffix("Hz").range(0.0..=f64::MAX).speed(1e6));
                     ui.end_row();
 
-                    ui.label("Min. SNR");
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Snr, "Min. SNR");
                     ui.add(DragValue::new(&mut self.snr_min).suffix(" dB"));
                     ui.end_row();
 
-                    ui.label("Noise figure");
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::TxPower, "Tx Power");
+                    ui.add(DragValue::new(&mut self.tx_power).suffix(" dBm"));
+                    ui.end_row();
+
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::RxPower, "Rx Power (sensitivity)");
+                    ui.add(DragValue::new(&mut self.rx_power).suffix(" dBm"));
+                    ui.end_row();
+
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::NoiseFigure, "Noise Figure");
                     ui.add(DragValue::new(&mut self.noise_figure).suffix(" dB"));
                     ui.end_row();
 
-                    ui.label("TX Gain");
-                    ui.add(DragValue::new(&mut self.tx_gain).suffix(" dBm"));
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::TxGain, "Tx Gain");
+                    ui.add(DragValue::new(&mut self.tx_gain).suffix(" dB"));
                     ui.end_row();
 
-                    ui.label("RX Gain");
-                    ui.add(DragValue::new(&mut self.rx_gain).suffix(" dBm"));
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::RxGain, "Rx Gain");
+                    ui.add(DragValue::new(&mut self.rx_gain).suffix(" dB"));
+                    ui.end_row();
+
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::AdditionalLosses, "AdditionalLosses");
+                    let loss_sum = self.additional_losses.iter().map(|l| l.loss).sum::<f64>();
+                    ui.label(format!("{:.2} dB", loss_sum));
                     ui.end_row();
                 })
             });
             frame_styled(ui).show(ui, |ui| {
+                if matches!(self.calculation_target, CalculationTarget::AdditionalLosses) {
+                    ui.disable();
+                }
                 ui.heading("Additional Losses");
                 ui.horizontal(|ui| {
                     ui.label("Add loss: ");
@@ -132,6 +189,23 @@ impl eframe::App for LinkBudgetApp {
                 if let Some(index) = delete_loss {
                     self.additional_losses.remove(index);
                 }
+            });
+            frame_styled(&ui).show(ui, |ui| {
+                ui.heading("Free Space Path loss");
+                egui::Grid::new("path_loss").show(ui, |ui| {
+                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Distance, "Distance");
+                    ui.add(DragValue::new(&mut self.distance).suffix(" m"));
+                    ui.end_row();
+
+                    ui.label("break distance");
+                    ui.add(DragValue::new(&mut self.d_break).suffix(" m"));
+                    ui.end_row();
+
+                    ui.label("break exponent");
+                    ui.add(DragValue::new(&mut self.break_exponent));
+                    ui.end_row();
+
+                });
             });
         });
     }
