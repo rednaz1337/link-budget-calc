d793c42dc48a087468d3b0129143d2f570f3a819
hz <hz@archlinux.local>
2024-11-28 09:49:16 +0100

    stuff

--- src/calc.rs
+++ src/calc.rs
@@ -1,24 +1,45 @@
-use uom::si::f64::*;
-use uom::si::velocity::meter_per_second;
-
-
-pub fn c() -> Velocity {
-    Velocity::new::<meter_per_second>(299792458.0)
+const KB: f64 = 1.380649e-23; // Boltzmann constant (joule per kelvin)
+const C: f64 = 299792458.0;
+pub fn lambda(frequency: f64) -> f64 {
+    C / frequency
 }
-pub fn lambda(frequency: Frequency) -> Length {
-    c() / frequency
+
+pub fn thermal_noise_power(temperature: f64, bandwidth: f64) -> f64 {
+    KB * temperature * bandwidth
 }
-mod friis {
-    use std::f64::consts::PI;
-    use uom::si::DimensionOne;
-    use uom::si::f64::*;
+
+pub fn thermal_noise_temperature(power: f64, bandwidth: f64) -> f64 {
+    power / bandwidth / KB
+}
+
+pub fn milliwatt_to_dbm(power: f64) -> f64 {
+    10.0 * f64::log10(power)
+}
+
+pub fn watt_to_dbm(power: f64) -> f64 {
+    10.0 * f64::log10(power * 1000.0)
+}
+
+pub fn dbm_to_milliwat(dbm: f64) -> f64 {
+    f64::powf(10.0, dbm / 10.)
+}
+
+pub fn dbm_to_watt(dbm: f64) -> f64 {
+    f64::powf(10.0, dbm / 10.) / 1000.0
+}
+pub mod friis {
     use crate::calc::lambda;
-    use uom::typenum::P2;
+    use std::f64::consts::PI;
 
-    pub fn path_loss(distance: Length, frequency: Frequency) -> Ratio {
-        ((lambda(frequency) /(4.0 * PI * distance)).powi(P2::new()))
-    }
-    pub fn rx_power(tx_power: Power, tx_gain: f64, rx_gain: f64, distance: Length, frequency: Frequency) -> Power {
-        tx_power * tx_gain * rx_gain * path_loss(distance, frequency)
+    pub fn path_loss(distance: f64, d_break: f64, frequency: f64, break_exponent: f64) -> f64 {
+        let one_meter_one_ghz = 32.0; // dB
+        let freq_loss = 20.0 * f64::log10(frequency / 1e9);
+        let path_loss = one_meter_one_ghz + freq_loss + if distance < d_break {
+            20.0 * f64::log10(distance / 1.0)
+        } else {
+            20.0 * f64::log10(d_break / 1.0) + break_exponent * 10.0 * f64::log10(distance / d_break)
+        };
+
+        return path_loss;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use std::error::Error;
 use eframe::{App, CreationContext, Frame};
-use egui::{Context, DragValue, Slider, TextEdit, Ui};
 use egui::CentralPanel;
+use egui::{Context, DragValue, Slider, TextEdit, Ui};
 use egui_extras::{Column, TableBuilder};
 use number_prefix::{NumberPrefix, Prefix};
-use uom::ConstantOp::Add;
-use uom::num::Num;
+use std::error::Error;
 
-mod calc;
 mod asynch;
+mod calc;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("Link Budget Calculator", native_options, Box::new(|cc| LinkBudgetApp::new(cc))).unwrap();
+    eframe::run_native(
+        "Link Budget Calculator",
+        native_options,
+        Box::new(|cc| LinkBudgetApp::new(cc)),
+    )
+    .unwrap();
 }
 
 #[derive(Default, PartialEq, Eq)]
@@ -31,18 +34,19 @@ enum CalculationTarget {
 }
 
 struct LinkBudgetApp {
-    temperature: f64, // Kelvin
-    bandwidth: f64, // Hertz
-    snr_min: f64, // dB
+    temperature: f64,  // Kelvin
+    frequency: f64,    // Hertz
+    bandwidth: f64,    // Hertz
+    snr_min: f64,      // dB
     noise_figure: f64, // dB
 
     tx_power: f64, // dBm
     rx_power: f64, // dBm
-    tx_gain: f64, // dB
-    rx_gain: f64, // dB
+    tx_gain: f64,  // dB
+    rx_gain: f64,  // dB
 
     distance: f64, // meter
-    d_break: f64, // meter
+    d_break: f64,  // meter
     break_exponent: f64,
 
     additional_losses: Vec<AdditionalLoss>,
@@ -58,6 +62,7 @@ impl Default for LinkBudgetApp {
             temperature: 290.0,
             bandwidth: 20e6,
             snr_min: 10.0,
+            frequency: 2.4e9,
             noise_figure: 10.0,
             tx_gain: 5.0,
             rx_gain: 5.0,
@@ -83,48 +88,121 @@ impl LinkBudgetApp {
     pub fn new(cc: &CreationContext) -> Result<Box<dyn App>, Box<dyn Error + Send + Sync>> {
         Ok(Box::new(Self::default()))
     }
+
+    pub fn additional_losses(&self) -> f64 {
+        self.additional_losses.iter().map(|l| l.loss).sum()
+    }
+
+    pub fn total_sum(&self) -> f64 {
+        let thermal =
+            calc::watt_to_dbm(calc::thermal_noise_power(self.temperature, self.bandwidth));
+        let additional = self.additional_losses();
+        let path = calc::friis::path_loss(self.distance, self.d_break, self.frequency, self.break_exponent);
+
+        let negative =
+             thermal
+            + additional
+            + path
+            + self.snr_min
+            + self.noise_figure
+            + self.rx_power;
+        let positive = self.tx_gain + self.tx_power + self.rx_gain;
+
+        return positive - negative;
+    }
 }
 
 impl eframe::App for LinkBudgetApp {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+        let total_db = self.total_sum();
         CentralPanel::default().show(ctx, |ui| {
             ui.heading("Link Budget Calculator");
             frame_styled(ui).show(ui, |ui| {
                 ui.heading("Base Info");
                 egui::Grid::new("base_data").num_columns(2).show(ui, |ui| {
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Temperature, "Temperature");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::Temperature,
+                        "Temperature",
+                    );
                     ui.add(DragValue::new(&mut self.temperature).suffix(" K"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Bandwidth, "Bandwidth");
-                    ui.add(prefix_drag_value(&mut self.bandwidth).suffix("Hz").range(0.0..=f64::MAX).speed(1e6));
+                    ui.label("Frequency");
+                    ui.add(
+                        prefix_drag_value(&mut self.frequency)
+                            .suffix("Hz")
+                            .range(0.0..=f64::MAX)
+                            .speed(1e6),
+                    );
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Snr, "Min. SNR");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::Bandwidth,
+                        "Bandwidth",
+                    );
+                    ui.add(
+                        prefix_drag_value(&mut self.bandwidth)
+                            .suffix("Hz")
+                            .range(0.0..=f64::MAX)
+                            .speed(1e6),
+                    );
+                    ui.end_row();
+
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::Snr,
+                        "Min. SNR",
+                    );
                     ui.add(DragValue::new(&mut self.snr_min).suffix(" dB"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::TxPower, "Tx Power");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::TxPower,
+                        "Tx Power",
+                    );
                     ui.add(DragValue::new(&mut self.tx_power).suffix(" dBm"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::RxPower, "Rx Power (sensitivity)");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::RxPower,
+                        "Rx Power (sensitivity)",
+                    );
                     ui.add(DragValue::new(&mut self.rx_power).suffix(" dBm"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::NoiseFigure, "Noise Figure");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::NoiseFigure,
+                        "Noise Figure",
+                    );
                     ui.add(DragValue::new(&mut self.noise_figure).suffix(" dB"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::TxGain, "Tx Gain");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::TxGain,
+                        "Tx Gain",
+                    );
                     ui.add(DragValue::new(&mut self.tx_gain).suffix(" dB"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::RxGain, "Rx Gain");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::RxGain,
+                        "Rx Gain",
+                    );
                     ui.add(DragValue::new(&mut self.rx_gain).suffix(" dB"));
                     ui.end_row();
 
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::AdditionalLosses, "AdditionalLosses");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::AdditionalLosses,
+                        "AdditionalLosses",
+                    );
                     let loss_sum = self.additional_losses.iter().map(|l| l.loss).sum::<f64>();
                     ui.label(format!("{:.2} dB", loss_sum));
                     ui.end_row();
@@ -137,9 +215,13 @@ impl eframe::App for LinkBudgetApp {
                 ui.heading("Additional Losses");
                 ui.horizontal(|ui| {
                     ui.label("Add loss: ");
-                    let name_response = ui.add(TextEdit::singleline(&mut self.loss_name).hint_text("Loss Name"));
+                    let name_response =
+                        ui.add(TextEdit::singleline(&mut self.loss_name).hint_text("Loss Name"));
                     ui.add(DragValue::new(&mut self.loss_db).suffix(" dB"));
-                    if ui.button("Add").clicked() || (name_response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))) {
+                    if ui.button("Add").clicked()
+                        || (name_response.lost_focus()
+                            && ui.input(|i| i.key_pressed(egui::Key::Enter)))
+                    {
                         if !self.loss_name.trim().is_empty() {
                             self.additional_losses.push(AdditionalLoss {
                                 name: self.loss_name.clone(),
@@ -147,7 +229,6 @@ impl eframe::App for LinkBudgetApp {
                             });
                             self.loss_name.clear();
                             self.loss_db = 0.0;
-
                         }
                     }
                 });
@@ -193,7 +274,11 @@ impl eframe::App for LinkBudgetApp {
             frame_styled(&ui).show(ui, |ui| {
                 ui.heading("Free Space Path loss");
                 egui::Grid::new("path_loss").show(ui, |ui| {
-                    ui.selectable_value(&mut self.calculation_target, CalculationTarget::Distance, "Distance");
+                    ui.selectable_value(
+                        &mut self.calculation_target,
+                        CalculationTarget::Distance,
+                        "Distance",
+                    );
                     ui.add(DragValue::new(&mut self.distance).suffix(" m"));
                     ui.end_row();
 
@@ -204,10 +289,24 @@ impl eframe::App for LinkBudgetApp {
                     ui.label("break exponent");
                     ui.add(DragValue::new(&mut self.break_exponent));
                     ui.end_row();
-
                 });
             });
         });
+
+        match self.calculation_target {
+            CalculationTarget::Temperature => {}
+            CalculationTarget::Bandwidth => {}
+            CalculationTarget::Snr => {
+                self.snr_min += total_db;
+            }
+            CalculationTarget::NoiseFigure => {}
+            CalculationTarget::TxGain => {}
+            CalculationTarget::RxGain => {}
+            CalculationTarget::Distance => {}
+            CalculationTarget::AdditionalLosses => {}
+            CalculationTarget::TxPower => {}
+            CalculationTarget::RxPower => {}
+        }
     }
 }
 
@@ -220,37 +319,38 @@ fn frame_styled(ui: &Ui) -> egui::Frame {
 }
 fn prefix_drag_value(value: &mut f64) -> DragValue {
     DragValue::new(value)
-        .custom_formatter(|value, range| {
-            match number_prefix::NumberPrefix::decimal(value) {
+        .custom_formatter(
+            |value, range| match number_prefix::NumberPrefix::decimal(value) {
                 NumberPrefix::Standalone(num) => {
                     format!("{num} ")
                 }
                 NumberPrefix::Prefixed(prefix, num) => {
                     format!("{:.1} {}", num, prefix)
                 }
-            }
-        }).custom_parser(|value| {
-        let Ok(number_prefix) = value.parse::<NumberPrefix<f64>>() else {
-            return None;
-        };
+            },
+        )
+        .custom_parser(|value| {
+            let Ok(number_prefix) = value.parse::<NumberPrefix<f64>>() else {
+                return None;
+            };
 
-        return match number_prefix {
-            NumberPrefix::Standalone(number) => { Some(number) }
-            NumberPrefix::Prefixed(prefix, number) => {
-                let factor = match prefix {
-                    Prefix::Kilo => { 1e3 }
-                    Prefix::Mega => { 1e6 }
-                    Prefix::Giga => { 1e9 }
-                    Prefix::Tera => { 1e12 }
-                    Prefix::Peta => { 1e15 }
-                    Prefix::Exa => { 1e18 }
-                    Prefix::Zetta => { 1e21 }
-                    Prefix::Yotta => { 1e24 }
-                    _ => return None
-                };
+            return match number_prefix {
+                NumberPrefix::Standalone(number) => Some(number),
+                NumberPrefix::Prefixed(prefix, number) => {
+                    let factor = match prefix {
+                        Prefix::Kilo => 1e3,
+                        Prefix::Mega => 1e6,
+                        Prefix::Giga => 1e9,
+                        Prefix::Tera => 1e12,
+                        Prefix::Peta => 1e15,
+                        Prefix::Exa => 1e18,
+                        Prefix::Zetta => 1e21,
+                        Prefix::Yotta => 1e24,
+                        _ => return None,
+                    };
 
-                Some(factor * number)
-            }
-        };
-    })
+                    Some(factor * number)
+                }
+            };
+        })
 }
